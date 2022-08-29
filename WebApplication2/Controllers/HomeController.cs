2a12169aec8f3504f523ba01309aed50da7a1a2f
Rafet <professionalraf@gmail.com>
2022-08-30 01:41:06 +0400

    Homework

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewBag.name = "Rafet";
+            //ViewData["Surname"] = "Memmedov";
+            //TempData["FullName"] = "Raf@Mammadoff";
+
+            Student studen1 = new Student("Rafet","Memmedov",23);
+            Student studen2 = new Student("Xeyal","Memmedov",23);
+            Student studen3 = new Student("Eli","Memmedov",23);
+
+            List<Student> students = new List<Student>();
+            students.Add(studen1);
+            students.Add(studen2);
+            students.Add(studen3);
+
+            return View(students);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public Student(string name,string surname,int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
